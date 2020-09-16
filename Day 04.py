4616659add378642aa1a4b96bd8bf30f8cde61b0
Rohit Singhal <68528567+Rohitsinghal-676@users.noreply.github.com>
2020-09-16 11:27:43 +0530

    done

--- Day 04.py
+++ Day 04.py
@@ -1,11 +1,29 @@
-N = int(input())
+class Person:
+    def __init__(self, initialAge):
+        if initialAge > 0:
+            self.age = initialAge
+        else:
+            print("Age is not valid, setting age to 0.")
+            self.age = 0
 
-if N % 2 != 0:
-    print("Weird")
-else:
-    if N <= 5:
-        print("Not Weird")
-    elif N <= 20:
-        print("Weird")
-    else:
-        print("Not Weird")
+    def amIOld(self):
+        if self.age < 13:
+            print("You are young.")
+        elif self.age < 18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+
+    def yearPasses(self):
+        self.age += 1
+
+
+t = int(input())
+for i in range(0, t):
+    age = int(input())
+    p = Person(age)
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()
+    p.amIOld()
+    print("")
