e2794955efa591af7c5a73b80d035ee43bb232b4
Rohit Singhal <68528567+Rohitsinghal-676@users.noreply.github.com>
2020-09-16 11:26:07 +0530

    Update Day 03.py

--- Day 03.py
+++ Day 03.py
@@ -1,9 +1,11 @@
-meal_cost = float(input())
-tip_percent = int(input())
-tax_percent = int(input())
+N = int(input())
 
-tip = meal_cost * tip_percent / 100
-tax = meal_cost * tax_percent / 100
-total = meal_cost + tip + tax
-
-print("The total meal cost is " + str(round(total)) + " dollars.")
+if N % 2 != 0:
+    print("Weird")
+else:
+    if N <= 5:
+        print("Not Weird")
+    elif N <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
